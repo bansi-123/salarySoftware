277c279a36fc7a639b28e067f18d128f51aa5244
sunandgoel <60604631+sunandgoel@users.noreply.github.com>
2021-08-21 12:57:32 +0530

    emailing

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var config = {};
 
-config.email = 'yofarhan@gmail.com' // Your Email here - Turn on less secure login from Google Security dashboard
+config.email = 'yofarhanrao@gmail.com' // Your Email here - Turn on less secure login from Google Security dashboard
 config.password = 'raofarhan100' // Your password here
 
 module.exports = config;
