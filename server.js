0bc42781c76e3845364df7be091b13b4f3cba077
sunandgoel <60604631+sunandgoel@users.noreply.github.com>
2021-08-24 10:36:56 +0530

    nothing

--- server.js
+++ server.js
@@ -24,12 +24,12 @@ require('./config/passport')(passport);
 
 //------------ MySQL Connection ------------//
 
-// const mysqldb = mysql.createConnection ({
-//     host: 'localhost',
-//     user: 'root',
-//     password: 'sunandroot',
-//     database: 'employees2'
-// });
+const mysqldb = mysql.createConnection ({
+    host: 'localhost',
+    user: 'root',
+    password: 'sunandroot',
+    database: 'employees2'
+});
 
 // const mysqldb = mysql.createConnection ({
 //     host: 'localhost',
@@ -38,12 +38,12 @@ require('./config/passport')(passport);
 //     database: 'employee' 
 // });
 
-const mysqldb = mysql.createConnection ({
-    host: 'localhost',
-    user: 'kshitij',
-    password: 'salary123',
-    database: 'employee'
-});
+// const mysqldb = mysql.createConnection ({
+//     host: 'localhost',
+//     user: 'kshitij',
+//     password: 'salary123',
+//     database: 'employee'
+// });
 
 // connect to database
 mysqldb.connect((err) => {
