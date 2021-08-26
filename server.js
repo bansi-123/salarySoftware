f2761544fb9c8a101523f7bc6ae65424a4db61e4
Kshitijde <kshitij.deshpande7@gmail.com>
2021-08-27 02:02:39 +0530

    late attendance logic completed and verified

--- server.js
+++ server.js
@@ -24,40 +24,26 @@ require('./config/passport')(passport);
 
 //------------ MySQL Connection ------------//
 
+// const mysqldb = mysql.createConnection ({
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'sunandroot',
+//     database: 'employees2'
+// });
+
+// const mysqldb = mysql.createConnection ({
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'Vineet@nexa1',
+//     database: 'employee' 
+// });
+
 const mysqldb = mysql.createConnection ({
     host: 'localhost',
-    user: 'root',
-    password: 'sunandroot',
-    database: 'employees2'
+    user: 'kshitij',
+    password: 'salary123',
+    database: 'employee'
 });
-
-// const mysqldb = mysql.createConnection ({
-//     host: 'localhost',
-//     user: 'root',
-//     password: 'Vineet@nexa1',
-//     database: 'employee' 
-// });
-
-// const mysqldb = mysql.createConnection ({
-//     host: 'localhost',
-//     user: 'root',
-//     password: 'Vineet@nexa1',
-//     database: 'employee' 
-// });
-
-// const mysqldb = mysql.createConnection ({
-//     host: 'localhost',
-//     user: 'root',
-//     password: 'Vineet@nexa1',
-//     database: 'employee' 
-// });
-
-// const mysqldb = mysql.createConnection ({
-//     host: 'localhost',
-//     user: 'kshitij',
-//     password: 'salary123',
-//     database: 'employee'
-// });
 
 // connect to database
 mysqldb.connect((err) => {
