a54b26fe884253a785eac2b114ba79d7282212cd
sunandgoel <60604631+sunandgoel@users.noreply.github.com>
2021-09-07 11:50:21 +0530

    view

--- server.js
+++ server.js
@@ -145,7 +145,7 @@ let router = require('./routes/excel.router.js');
 app.use(express.static('resources'));
 app.use('/uploadcsv', router); 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server running on PORT ${PORT}`)
 );
