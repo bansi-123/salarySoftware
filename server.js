bc68f6b9b63626167058aaec340099a92d40d782
sunandgoel <60604631+sunandgoel@users.noreply.github.com>
2021-10-04 22:41:58 +0530

    all months opened for testing in generate salary

--- server.js
+++ server.js
@@ -24,26 +24,27 @@ require('./config/passport')(passport);
 
 //------------ MySQL Connection ------------//
 
-// const mysqldb = mysql.createConnection ({
-//     host: 'localhost',
-//     user: 'root',
-//     password: 'sunandroot',
-//     database: 'employee'
-// });
-
-// const mysqldb = mysql.createConnection ({
-//     host: 'localhost',
-//     user: 'root',
-//     password: 'sunandroot',
-//     database: 'employee'
-// });
-
 const mysqldb = mysql.createConnection ({
     host: 'localhost',
     user: 'root',
-    password: 'Vineet@nexa1',
-    database: 'employee' 
+    password: 'sunandroot',
+    multipleStatements: true,
+    database: 'employee'
 });
+
+// const mysqldb = mysql.createConnection ({
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'sunandroot',
+//     database: 'employee'
+// });
+
+// const mysqldb = mysql.createConnection ({
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'Vineet@nexa1',
+//     database: 'employee' 
+// });
 
 
 // const mysqldb = mysql.createConnection ({
