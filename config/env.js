8d6b783efd92af1b1e04f2834b48173d60c8ae0d
Nidhi Kowtal <kowtalnidhi@gmail.com>
2021-10-07 15:00:48 +0530

    merging

--- config/env.js
+++ config/env.js
@@ -13,38 +13,10 @@
 // };
 
 
-// const env = {
-//   database: 'employee',
-//   username: 'root',
-//   password: 'Vineet@nexa1',
-//   host: 'localhost',
-//   dialect: 'mysql',
-//   pool: {
-//     max: 5,
-//     min: 0,
-//     acquire: 30000,
-//     idle: 10000
-//   }
-// };
-
-// const env = {
-//   database: 'employee',
-//   username: 'root',
-//   password: 'Vineet@nexa1',
-//   host: 'localhost',
-//   dialect: 'mysql',
-//   pool: {
-//     max: 5,
-//     min: 0,
-//     acquire: 30000,
-//     idle: 10000
-//   }
-// };
-
 const env = {
   database: 'employee',
-  username: 'kshitij',
-  password: 'salary123',
+  username: 'root',
+  password: 'Vineet@nexa1',
   host: 'localhost',
   dialect: 'mysql',
   pool: {
@@ -54,5 +26,33 @@ const env = {
     idle: 10000
   }
 };
+
+// const env = {
+//   database: 'employee',
+//   username: 'root',
+//   password: 'Vineet@nexa1',
+//   host: 'localhost',
+//   dialect: 'mysql',
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000
+//   }
+// };
+
+// const env = {
+//   database: 'employee',
+//   username: 'kshitij',
+//   password: 'salary123',
+//   host: 'localhost',
+//   dialect: 'mysql',
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000
+//   }
+// };
  
 module.exports = env;
